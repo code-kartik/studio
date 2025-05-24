fb5579586b39057a691f8d440e8de983f31677e5
Kartik Arora <kartikarora23082003@gmail.com>
2025-05-24 16:29:40 +0000

    lexitrack on github

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   eslint: {
     ignoreDuringBuilds: true,
@@ -18,6 +18,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'export',
 };
 
 export default nextConfig;
